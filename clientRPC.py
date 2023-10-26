c240e560f04e56b5fbe68d24af1103e194aec238
Rictrix10 <ricardocerqueiragoncalves10@gmail.com>
2023-10-26 12:40:23 +0100

    Permite escolher rotação da imagem

--- clientRPC.py
+++ clientRPC.py
@@ -1,14 +1,14 @@
 import xmlrpc.client
 import base64
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, simpledialog
 from PIL import Image, ImageTk
 
 def browse_image():
     file_path = filedialog.askopenfilename()
     if file_path:
         image_file_path.set(file_path)
-        show_selected_image(file_path)  # Exibe a imagem selecionada
+        show_selected_image(file_path)
 
 def show_selected_image(image_path):
     image = Image.open(image_path)
@@ -42,15 +42,28 @@ def process_image(operation):
     if operation == "convert":
         grayscale_image = convert_to_grayscale(image_path)
         grayscale_image.save('grayscale_image.jpg', format="JPEG")
-    if operation == "resize":
-        resized_image = resize_image(image_path, 300, 200)
-        resized_image.save('resized_image.jpg', format="JPEG")
-    if operation == "rotate":
-        rotated_image = rotate_image(image_path, 45)
-        rotated_image.save('rotated_image.jpg', format="JPEG")
+        result_label.config(text="Imagem em tons de cinza salva com sucesso!")
+    elif operation == "resize":
+        # Solicitar largura e altura ao usuário
+        width = simpledialog.askinteger("Redimensionar imagem", "Insira a largura desejada:")
+        height = simpledialog.askinteger("Redimensionar imagem", "Insira a altura desejada:")
 
-    result_label.config(text="Imagens processadas e salvas com sucesso!")
+        if width is not None and height is not None:
+            resized_image = resize_image(image_path, width, height)
+            resized_image.save('resized_image.jpg', format="JPEG")
+            result_label.config(text="Imagem redimensionada e salva com sucesso!")
+        else:
+            result_label.config(text="Largura e altura inválidas.")
+    elif operation == "rotate":
+        # Solicitar o ângulo de rotação ao usuário
+        angle = simpledialog.askinteger("Rodar imagem", "Insira o ângulo de rotação desejado (em graus):")
 
+        if angle is not None:
+            rotated_image = rotate_image(image_path, angle)
+            rotated_image.save('rotated_image.jpg', format="JPEG")
+            result_label.config(text="Imagem rotacionada e salva com sucesso!")
+        else:
+            result_label.config(text="Ângulo de rotação inválido.")
 
 root = tk.Tk()
 root.title("Cliente de Processamento de Imagem")
@@ -62,19 +75,17 @@ result_label.pack()
 file_button = tk.Button(root, text="Escolher Imagem", command=browse_image)
 file_button.pack()
 
-process_button = tk.Button(root, text="Converter imagem para cinza", command=lambda: process_image("convert"))
-process_button.pack()
+process_button_convert = tk.Button(root, text="Converter imagem para cinza", command=lambda: process_image("convert"))
+process_button_convert.pack()
 
-process_button = tk.Button(root, text="Redimensionar imagem", command=lambda: process_image("resize"))
-process_button.pack()
+process_button_resize = tk.Button(root, text="Redimensionar imagem", command=lambda: process_image("resize"))
+process_button_resize.pack()
 
-process_button = tk.Button(root, text="Rodar imagem", command=lambda: process_image("rotate"))
-process_button.pack()
-
+process_button_rotate = tk.Button(root, text="Rodar imagem", command=lambda: process_image("rotate"))
+process_button_rotate.pack()
 
 # Adicione um widget de label para exibir a imagem selecionada
 image_label = tk.Label(root)
 image_label.pack()
 
 root.mainloop()
-
